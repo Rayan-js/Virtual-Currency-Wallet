4d663b210193cee2a076b58ceb916bfe4ac85b35
Ubuntu <ubuntu@ip-172-31-46-108.sa-east-1.compute.internal>
2024-11-21 18:01:03 +0000

    second contente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // Conexão com PostgreSQL
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', // ou o nome do serviço no Docker Compose
+      port: 5432,
+      username: 'seu_usuario',
+      password: 'sua_senha',
+      database: 'virtual_currency_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Não recomendado em produção
+    }),
+    // Conexão com MongoDB
+    MongooseModule.forRoot('mongodb://localhost:27017/virtual_currency_logs'),
+    UsersModule,
+  ],
 })
 export class AppModule {}
+
