ae94fad2f0793ef6f7e3da2dc6e4c3c6327b0d31
fitinsur-rayan <fitinsur-rayan@fitinsur.io>
2024-11-21 15:13:53 -0300

    add user entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Transaction } from '../transactions/transaction.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  nome: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ type: 'decimal', default: 0, precision: 10, scale: 2 })
+  saldo: number;
+
+  @OneToMany(() => Transaction, (transaction) => transaction.user)
+  transactions: Transaction[];
+}
