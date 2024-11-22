ee21835e86b70acfb625fb074c4af357cfd9dde1
fitinsur-rayan <fitinsur-rayan@fitinsur.io>
2024-11-22 10:38:52 -0300

    Add hello world

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,33 +2,35 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [
-	ConfigModule.forRoot({
-	      isGlobal: true, // Torna as variáveis de ambiente acessíveis em todo o projeto
-	    }),
-	// Conexão com PostgreSQL
-	TypeOrmModule.forRoot({
-	  type: 'postgres',
-	  host: process.env.DATABASE_HOST,
-	  port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-	  username: process.env.DATABASE_USER,
-	  password: process.env.DATABASE_PASSWORD,
-	  database: process.env.DATABASE_NAME,
-	  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-	  synchronize: true,
-	}),   
- 
- 
+    ConfigModule.forRoot({
+      isGlobal: true, // Torna as variáveis de ambiente acessíveis em todo o projeto
+    }),
+    // Conexão com PostgreSQL
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+
     // Conexão com MongoDB
     MongooseModule.forRoot(process.env.MONGODB_URI),
 
     UsersModule,
     TransactionsModule,
   ],
+  controllers: [AppController], // Adiciona o AppController
+  providers: [AppService], // Adiciona o AppService
 })
 export class AppModule {}
-
