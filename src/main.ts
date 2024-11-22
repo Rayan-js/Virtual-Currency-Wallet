27328093008592b8a1527dc2d698125ec1061db7
fitinsur-rayan <fitinsur-rayan@fitinsur.io>
2024-11-22 19:35:36 -0300

    Add cords permission

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import { AppModule } from './app.module'; // Adjust the path if necessary
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: '*', // Permite todas as origens. Troque por domínios específicos para maior segurança.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
   // main.ts
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
